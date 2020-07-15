880fdc848418fd6645c4c423bf94150e429af897
divyanarendran <64362020+divyanarendran@users.noreply.github.com>
2020-07-15 19:50:07 +0530

    Add files via upload

---
+++ week3_day9_AngularSuperCalculator.java
@@ -0,0 +1,41 @@
+package days;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class week3_day9_AngularSuperCalculator {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("https://juliemr.github.io/protractor-demo/");
+		Thread.sleep(2000);
+		
+		//give input1
+		WebElement input1=driver.findElementByXPath("//input[@ng-model='first']");
+		input1.sendKeys("5");
+		//mul
+		driver.findElementByXPath("//select[@ng-model='operator']").click();
+		driver.findElementByXPath("//option[@value='MULTIPLICATION']").click();
+		//give input2
+		driver.findElementByXPath("//input[@ng-model='second']").sendKeys("5");
+		driver.findElementById("gobutton").click();
+		WebDriverWait wait;
+		WebElement answer=driver.findElementByXPath("//h2[@class='ng-binding']");
+		wait= new WebDriverWait(driver, 20);
+		//wait.until(ExpectedConditions.visibilityOf(answer));
+		//wait.until(ExpectedConditions.textToBePresentInElement(answer,	));
+		Actions builder= new Actions(driver);
+		builder.pause(2000).perform();		
+		System.out.println(answer.getText());
+		//System.out.println("its wrong");
+		
+		
+	}
+
+}
